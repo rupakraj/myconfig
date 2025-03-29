faccdaf1f474337079059ddd86268cf5ba20d70d
rughimire <rughimire@gmail.com>
2025-03-29 21:32:13 +0545

    nvim: comment for dynamic comment

---
+++ nvim/lua/plugins/others.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "numToStr/Comment.nvim",
+        opts = { },
+        config = function ()
+            require("Comment").setup(opts)
+        end
+    },
+
+--     {
+--         "andrewferrier/wrapping.nvim",
+--         config = function()
+--             require("wrapping").setup({
+--                 auto_set_mode_filetype_allowlist = {
+--                     "asciidoc",
+--                     "gitcommit",
+--                     "latex",
+--                     "mail",
+--                     "markdown",
+--                     "rst",
+--                     "tex",
+--                     "text",
+--                     "org",
+--                 },
+--             })
+-- 
+--             local commander = require("commander")
+--             commander.add({
+--                 {
+--                     desc = "Line wrapping toggle (soft)",
+--                     cmd  = ":SoftWrapMode<CR>",
+--                     keys = { "n", "[ow"}
+--                 },
+--             })
+--         end
+--     }
+}
