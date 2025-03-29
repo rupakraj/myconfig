87c118429a3a14f80f6820e461094fc8bd99e7a0
rughimire <rughimire@gmail.com>
2025-03-29 21:30:19 +0545

    nvim: theme recover to catppuccin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,8 +4,8 @@ return {
          config = function()
              require('lualine').setup({
                  options = {
- --                     theme = 'dracula',
---                      theme = 'gruvbox_dark',
+                     theme = 'dracula',
+                     -- theme = 'gruvbox_dark',
                      icons_enabled = true,
                      always_show_tabline = true,
                  }
