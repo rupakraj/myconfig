54eb6d1a7fac17b33d371854bab2b2d07f2a449d
rughimire <rughimire@gmail.com>
2025-04-03 11:17:19 +0545

    nvim : folding fix

---
+++ nvim/lua/plugins/folding.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+
+        config = function()
+            ufo = require('ufo')
+
+            vim.o.foldcolumn = '1' -- '0' is not bad
+            vim.o.foldlevel = 5 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+            vim.o.fillchars = "foldopen:,foldclose:,foldsep:│,fold: "
+            -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+            -- vim.o.statuscolumn = '%=%l%s%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "▼" : "⏵") : " " }'
+            vim.o.statuscolumn='%=%l%s%{foldlevel(v:lnum) > 0 ? (foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "-" : "+") : "│") : " " }'
+
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+            vim.keymap.set('n', 'zR', ufo.openAllFolds)
+            vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+
+            ufo.setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return {'treesitter', 'indent'}
+                end
+            })
+        end
+    },
+}
