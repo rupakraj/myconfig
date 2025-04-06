546123736c8cdb768a3358bec807a6fd0fd56cdb
rughimire <rughimire@gmail.com>
2025-04-06 12:09:39 +0545

    nvim: smart indent, mouse, signcolumn

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -15,8 +15,23 @@ vim.cmd("set linebreak")
 vim.cmd("set breakindent")
 vim.cmd("set textwidth=80")
 
+-- vim options
+vim.opt.mouse = "a"
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+
+
 -- clipboard share with OS
 vim.opt.clipboard = "unnamedplus"
+
+-- experimental font
+-- vim.opt.guifont = "DejaVu Sans Mono:h12"
+-- experimental ends
+
+
+-- git
+vim.g.signcolumn="yes"
+
 
 -- vim.opts.rocks.enabled = false
 vim.filetype.add({
